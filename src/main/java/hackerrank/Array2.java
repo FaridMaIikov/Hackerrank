e3a12017562643daf7062d928cf3c0b3518ae0c8
Farid <malikow.farid@gmail.com>
2023-06-15 17:44:55 +0400

    Negative array

---
+++ src/main/java/hackerrank/Array2.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Array2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            int sum = 0;
+            for (int j = i; j < size; j++) {
+                sum += arr[j];
+
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
