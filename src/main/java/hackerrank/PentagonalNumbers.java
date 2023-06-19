3b25f8e8650715903f0d89f7a822d9d26aa5c1bf
Farid <malikow.farid@gmail.com>
2023-06-19 12:10:29 +0400

    Pentagonal numbers with method

---
+++ src/main/java/hackerrank/PentagonalNumbers.java
@@ -0,0 +1,27 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class PentagonalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int[] pentagonalNumbers = getPentagonalNumbers(10);
+
+        for (int nums : pentagonalNumbers) {
+            System.out.println(nums);
+        }
+
+    }
+
+
+    public static int[] getPentagonalNumbers(int input) {
+        int[] numbers = new int[input];
+
+        for (int i = 1; i <= input; i++) {
+            int b = (int) (3 * (Math.pow(i, 2)) - i) / 2;
+            numbers[i - 1] = b;
+        }
+        return numbers;
+    }
+}
