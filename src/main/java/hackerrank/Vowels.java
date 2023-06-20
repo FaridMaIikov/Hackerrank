785114816dcb79fd6f015f4404ae9c647e7a50d5
Farid <malikow.farid@gmail.com>
2023-06-21 00:10:46 +0400

    Methods

---
+++ src/main/java/hackerrank/Vowels.java
@@ -0,0 +1,26 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Vowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int findVowels=getFindVowels(word);
+        System.out.println(findVowels);
+
+    }
+
+    public static int getFindVowels(String word) {
+        word = word.toLowerCase();
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char character = word.charAt(i);
+            if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
