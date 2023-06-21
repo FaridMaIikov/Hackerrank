0fac8241588c4f9c13dcc83a457bf3470511d56f
Farid <malikow.farid@gmail.com>
2023-06-21 10:53:57 +0400

    Changes

--- src/main/java/hackerrank/MiddleCharacter.java
+++ src/main/java/hackerrank/MiddleCharacter.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class MiddleCharacter {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String word=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
         getMiddleCharacter(word);
 
     }
 
-    public static void getMiddleCharacter(String str){
-        int length=str.length();
-        int middle=length/2;
+    public static void getMiddleCharacter(String str) {
+        int length = str.length();
+        int middle = length / 2;
 
-        if(length/2==0){
-            char middleChar=str.charAt(middle-1);
-            System.out.println("Middle character: "+middleChar);
-        }else {
-            char middleChar=str.charAt(middle);
-            char middleChar1=str.charAt(middle+1);
-            System.out.println("Middle character: "+middleChar+middleChar1);
+        if (length % 2 == 0) {
+            char middleChar = str.charAt(middle - 1);
+            System.out.println("Middle character: " + middleChar);
+        } else {
+            char middleChar = str.charAt(middle);
+            char middleChar1 = str.charAt(middle + 1);
+            System.out.println("Middle character: " + middleChar + middleChar1);
         }
     }
 }
