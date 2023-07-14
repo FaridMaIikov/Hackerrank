a17715a443329d29f83cc7b3300942a3d2211c7b
Farid <malikow.farid@gmail.com>
2023-07-14 23:06:28 +0400

    Romp added

--- src/main/java/hackerrank/Romb.java
+++ src/main/java/hackerrank/Romb.java
@@ -4,7 +4,32 @@ import java.util.Scanner;
 
 public class Romb {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printDiamondPattern(n);
     }
 
+    public static void printDiamondPattern(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            printRow(n, i);
+        }
+
+
+        for (int i = n - 1; i >= 1; i--) {
+            printRow(n, i);
+        }
+    }
+
+    public static void printRow(int n, int row) {
+        for (int i = 1; i <= n - row; i++) {
+            System.out.print(" ");
+        }
+
+        for (int i = 1; i <= 2 * row - 1; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
 }
