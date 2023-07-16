4089bc57b6345efbc0779eb9dbedaf26aa300f59
Farid <malikow.farid@gmail.com>
2023-07-16 15:55:54 +0400

    Change

--- src/main/java/hackerrank/Loops.java
+++ src/main/java/hackerrank/Loops.java
@@ -6,9 +6,9 @@ public class Loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= 10; ++i) {
+        for (int i = 1; i <= 10; i++) {
             int result = i * n;
-            System.out.println(n + " x " + i + " = " + result);
+            System.out.printf("%s*%s=%s\n",i,n,result);
         }
     }
 }
